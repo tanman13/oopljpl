d454bb3ccd670cade261a4e9d17bbf7b4b7714b1
Glenn Downing <downing@cs.utexas.edu>
2015-01-15 14:03:58 -0800

    another commit

---
+++ examples/Sequences.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+
+# ------------
+# Sequences.py
+# ------------
+
+def test_1 (c) :
+    assert not c()
+
+    a = c("01234")
+    assert len(a) == 5
+
+    a = c("01234")
+    assert a[2] == "2"
+    try :
+        assert (a[5] == 0)                             # index error
+        assert False
+    except IndexError as e :
+        assert type(e.args)    is tuple
+        assert len(e.args)     == 1
+        assert e.args[0][-18:] == "index out of range"
+
+    a = c("54321")
+    assert a[-3] == "3"
+    try :
+        assert (a[-6] == 0)                            # index error
+        assert False
+    except IndexError as e :
+        assert type(e.args)    is tuple
+        assert len(e.args)     == 1
+        assert e.args[0][-18:] == "index out of range"
+
+    a = c("01234")
+    i = iter(a)
+    assert str(type(i))[-10:-2] == "iterator"
+    assert i is iter(i)
+
+    a = c("01234")
+    assert "1"     in a
+    assert "5" not in a
+
+    a = c("01234")
+    assert not (a != a)
+    assert     (a == a)
+    assert not (a <  a)
+    assert     (a <= a)
+    assert not (a >  a)
+    assert     (a >= a)
+
+    a = c("01234")
+    assert (a + a) == c("0123401234")
+    b  = c("01234")
+    b += a
+    assert b == c("0123401234")
+
+    a = c("01234")
+    assert (3 * a) == c("012340123401234")
+    b  = c("01234")
+    b *= 3
+    assert b == c("012340123401234")
+
+    a = c("01234")
+    assert a[ 1: 4] == c("123")
+    assert a[-4:-1] == c("123")
+    assert a[ 1:  ] == c("1234")
+    assert a[-4:  ] == c("1234")
+    assert a[  : 4] == c("0123")
+    assert a[  :-1] == c("0123")
+    assert a[ 0: 5] == c("01234")
+    assert a[-5: 5] == c("01234")
+    assert a[-9: 9] == c("01234")
+    assert a[  :  ] == c("01234")
+    assert a[ 4: 1] == c("")
+
+    a = c("01234")
+    assert a[ 1: 4: 2] == c("13")
+    assert a[-4:-1: 2] == c("13")
+    assert a[ 1:  : 2] == c("13")
+    assert a[-4:  : 2] == c("13")
+    assert a[  : 4: 2] == c("02")
+    assert a[  :-1: 2] == c("02")
+    assert a[ 0: 5: 2] == c("024")
+    assert a[-5: 5: 2] == c("024")
+    assert a[-9: 9: 2] == c("024")
+    assert a[  :  : 2] == c("024")
+    assert a[ 4: 1: 2] == c("")
+
+    a = c("01234")
+    assert a[ 4: 1:-2] == c("42")
+    assert a[-1:-4:-2] == c("42")
+    assert a[ 4:  :-2] == c("420")
+    assert a[-1:  :-2] == c("420")
+    assert a[  : 1:-2] == c("42")
+    assert a[  :-4:-2] == c("42")
+    assert a[ 4:-6:-2] == c("420")
+    assert a[-1:-6:-2] == c("420")
+    assert a[ 9:-9:-2] == c("420")
+    assert a[  :  :-2] == c("420")
+    assert a[ 1: 4:-2] == c("")
+
+    a = c("01234")
+    assert a[ :  :-1] == c("43210")
+    assert a[4:-6:-1] == c("43210")
+
+def test_2 (c) :
+    a = c([2, 3, 4])
+    assert sum(a, 0) == 9
+    assert sum(a)    == 9
+
+    a = c([[2, 3, 4], [5, 6]])
+    assert sum(a, []) == [2, 3, 4, 5, 6]
+#   assert sum(a)     == [2, 3, 4, 5, 6] # TypeError: unsupported operand type(s) for +: 'int' and 'list'
+
+    a = c([(2, 3, 4), (5, 6)])
+    assert sum(a, ()) == (2, 3, 4, 5, 6)
+#   assert sum(a)     == (2, 3, 4, 5, 6) # TypeError: unsupported operand type(s) for +: 'int' and 'tuple'
+
+    a = c(["abc", "de"])
+#   assert sum(a, "") == "abcde" # TypeError: sum() can't sum strings [use "".join(seq) instead]
+    assert "".join(a) == "abcde"
+
+print("Sequences.py")
+
+test_1(str)
+test_1(list)
+test_1(tuple)
+
+test_2(list)
+test_2(tuple)
+
+print("Done.")
---
+++ examples/Sets.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+
+# -------
+# Sets.py
+# -------
+
+print("Sets.py")
+
+s = {2, 3.45, "abc", 2}
+assert type(s) is     set
+assert len(s)  ==     3
+assert s       is not {2, 3.45, 'abc'}
+assert s       ==     {2, 3.45, 'abc'}
+assert s       ==     {2, 'abc', 3.45}
+assert s       ==     frozenset((2, 3.45, 'abc'))
+
+s = frozenset([2, 3.45, "abc", 2])
+assert type(s)      is     frozenset
+assert len(s)       ==     3
+assert s            is not frozenset([2, 3.45, 'abc'])
+assert s            ==     frozenset([2, 3.45, 'abc'])
+assert s            ==     frozenset((2, 'abc', 3.45))
+assert s            ==     {2, 3.45, 'abc'}
+
+s = set({2 : "ghi", 3.45 : 3, "abc" : 6.78, 2 : "def"})
+assert type(s) is set
+assert s       == {2, 3.45, "abc"}
+
+s = frozenset({2 : "ghi", 3.45 : 3, "abc" : 6.78, 2 : "def"})
+assert type(s) is frozenset
+assert s       == frozenset([2, 3.45, "abc"])
+
+set()       is not set()
+set()       ==     set()
+frozenset() is     frozenset()
+
+s = {2, 3.45, "abc", 2}
+t = set(s)
+assert s is not t
+assert s ==     t
+
+s = frozenset([2, 3.45, "abc", 2])
+t = frozenset(s)
+assert s is t
+
+s = {2, 3.45, "abc"}
+t = s.copy()
+assert s is not t
+assert s ==     t
+
+s = frozenset([2, 3.45, "abc"])
+t = s.copy()
+assert s is t
+
+s = {2, 3.45, "abc"}
+t = s
+assert s is t
+s |= frozenset([6])
+assert s == set([2, 3.45, "abc", 6])
+assert s is t
+
+s = frozenset([2, 3.45, "abc"])
+t = s
+assert s is t
+s |= frozenset([6])
+assert s == frozenset([2, 3.45, "abc", 6])
+assert t == frozenset([2, 3.45, "abc"])
+
+s = set()
+s.add(2)
+s.add(3.45)
+s.add("abc")
+s.add(2)
+assert s == {2, 3.45, "abc"}
+s.remove("abc")
+assert s == {2, 3.45}
+try :
+    s.remove("abc")
+    assert False
+except KeyError as e:
+    assert type(e.args) is tuple
+    assert len(e.args)  == 1
+    assert e.args       == ("abc",)
+
+s = frozenset([2, 3.45, "abc"])
+assert 2     in s
+assert 3 not in s
+
+s = frozenset([1, 3])
+t = frozenset([1, 3, 5, 7])
+assert s <  t              # proper subset
+assert s <= t
+assert t >  s              # proper superset
+assert t >= s
+
+s = frozenset([1, 2, 3, 5])
+t = frozenset([1, 2, 4, 6])
+assert (s | t) == frozenset([1, 2, 3, 4, 5, 6]) # union
+assert (t | s) == frozenset([1, 2, 3, 4, 5, 6])
+assert (s & t) == frozenset([1, 2])             # intersection
+assert (t & s) == frozenset([1, 2])
+assert (s - t) == frozenset([3, 5])             # difference
+assert (t - s) == frozenset([4, 6])
+assert (s ^ t) == frozenset([3, 5, 4, 6])       # symmetric difference
+assert (t ^ s) == frozenset([3, 5, 4, 6])
+
+assert False is not 0
+assert False ==     0
+assert True  is not 1
+assert True  ==     1
+assert 2     is not 2.0
+assert 2     ==     2.0
+s = {False, 0, True, 1, 2, 2.0}
+assert s == {False, True, 2}
+assert s == {0, 1, 2}
+
+class A :
+    pass
+
+s = {A(), A()}
+assert len(s) == 2
+
+s = {"abc",             "def"}
+s = {(2, 3),            (4, 5)}
+s = {frozenset([2, 3]), frozenset([4, 5])}
+
+#s = {set([2, 3]),            set([4, 5])}            # TypeError: set objects are unhashable
+#s = {[2, 3],                 [4, 5]}                 # TypeError: list objects are unhashable
+#s = {{"abc" : 2, "def" : 3}, {"ghi" : 4, "jkl" : 5}} # TypeError: unhashable type: 'dict'
+
+print("Done.")
