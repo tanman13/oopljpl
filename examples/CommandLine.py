fd3387078b2c44408d1f123ec983c2418fef9d0a
Glenn Downing <downing@cs.utexas.edu>
2015-01-15 10:43:51 -0800

    another commit

--- examples/CommandLine.py
+++ examples/CommandLine.py
@@ -16,8 +16,10 @@ print("CommandLine.py")
 assert type(sys)   is types.ModuleType
 assert type(types) is types.ModuleType
 
-assert type(sys.argv) is     list
-assert sys.argv       is not ["./CommandLine.py", "Nothing", "to", "be", "done."]
-assert sys.argv       ==     ["./CommandLine.py", "Nothing", "to", "be", "done."]
+assert type(sys.argv) is list
+assert len(sys.argv)  == 5
+
+assert sys.argv is not ["./CommandLine.py", "Nothing", "to", "be", "done."]
+assert sys.argv ==     ["./CommandLine.py", "Nothing", "to", "be", "done."]
 
 print("Done.")
--- examples/FileInputOutput.py
+++ examples/FileInputOutput.py
@@ -7,9 +7,15 @@
 import sys
 import types
 
-assert type(sys)      is types.ModuleType
+assert type(sys)   is types.ModuleType
+assert type(types) is types.ModuleType
+
 assert type(sys.argv) is list
 assert len(sys.argv)  == 1
+
+assert sys.argv is not ["./FileInputOutput.py"]
+assert sys.argv ==     ["./FileInputOutput.py"]
+
 f = open(sys.argv[0])
 
 for v in f :
