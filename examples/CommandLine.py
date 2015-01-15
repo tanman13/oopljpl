cd74442de9124bdb2a183ef4ce23afc622aa50f7
Glenn Downing <downing@cs.utexas.edu>
2015-01-15 10:39:25 -0800

    another commit

---
+++ examples/CommandLine.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# --------------
+# CommandLine.py
+# --------------
+
+"""
+% CommandLine.py Nothing to be done.
+"""
+
+import sys
+import types
+
+print("CommandLine.py")
+
+assert type(sys)   is types.ModuleType
+assert type(types) is types.ModuleType
+
+assert type(sys.argv) is     list
+assert sys.argv       is not ["./CommandLine.py", "Nothing", "to", "be", "done."]
+assert sys.argv       ==     ["./CommandLine.py", "Nothing", "to", "be", "done."]
+
+print("Done.")
--- exercises/Count2.py
+++ exercises/Count2.py
@@ -8,7 +8,7 @@ from itertools import count
 from unittest  import TestCase, TestLoader, TestSuite, TextTestRunner
 
 class my_count () :
-    def __init__ (self, b, s = 1) :
+    def __init__ (self, b = 0, s = 1) :
         self.b = b
         self.s = s
 
@@ -23,18 +23,25 @@ class my_count () :
 def bind (c) :
     class MyUnitTests (TestCase) :
         def test_1 (self) :
+            p = c()
+            assert iter(p) is p
+            assert next(p) == 0
+            assert next(p) == 1
+            assert next(p) == 2
+
+        def test_2 (self) :
             p = c(2)
             assert iter(p) is p
             assert next(p) == 2
             assert next(p) == 3
             assert next(p) == 4
 
-        def test_2 (self) :
-            p = c(2, 3)
+        def test_3 (self) :
+            p = c(5, 2)
             assert iter(p) is p
-            assert next(p) == 2
             assert next(p) == 5
-            assert next(p) == 8
+            assert next(p) == 7
+            assert next(p) == 9
     return MyUnitTests
 
 if __name__ == "__main__" :
