4166cf8313dc5562a0c787412623cb0c36292f9e
Glenn Downing <downing@cs.utexas.edu>
2015-01-16 12:56:24 -0800

    another commit

--- exercises/TestCollatz.py
+++ exercises/TestCollatz.py
@@ -2,19 +2,31 @@
 
 from unittest import main, TestCase
 
-from Collatz import max_cycle_length
+from Collatz import cycle_length, max_cycle_length
 
 class MyUnitTests (TestCase) :
-    def test_eval_1 (self) :
+    def test_cycle_length_1 (self) :
+        self.assertRaises(AssertionError, cycle_length, 0)
+
+    def test_cycle_length_2 (self) :
+        self.assertEqual(cycle_length(1), 1)
+
+    def test_cycle_length_3 (self) :
+        self.assertEqual(cycle_length(5), 6)
+
+    def test_cycle_length_4 (self) :
+        self.assertEqual(cycle_length(10), 7)
+
+    def test_max_cycle_length_1 (self) :
         self.assertEqual(max_cycle_length(1, 10), 20)
 
-    def test_eval_2 (self) :
+    def test_max_cycle_length_2 (self) :
         self.assertEqual(max_cycle_length(100, 200), 125)
 
-    def test_eval_3 (self) :
+    def test_max_cycle_length_3 (self) :
         self.assertEqual(max_cycle_length(201, 210), 89)
 
-    def test_eval_4 (self) :
+    def test_max_cycle_length_eval_4 (self) :
         self.assertEqual(max_cycle_length(900, 1000), 174)
 
 if __name__ == "__main__" :
