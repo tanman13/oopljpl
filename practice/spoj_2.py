b1576918034166124a897559497faa8034d0f6ed
Ian Tan <ian.tan@jpl.nasa.gov>
2015-02-04 13:55:32 -0800

    Moved partner practice exercises to a new folder.
    Moved all post-class practice exercises to the "practice" folder.

---
+++ practice/spoj_2.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+from sys import stdin
+
+from math     import sqrt
+from unittest import main, TestCase
+
+d = {}
+
+def is_prime (n) :
+    assert n > 0
+    if n in d :
+      return d[n]
+    
+    if n == 1 :
+        d[n] = False
+        return False
+    if n == 2 :
+        d[n] = True
+        return True
+    if (n % 2) == 0 :
+        d[n] = False
+        return False
+    for i in range(3, int(sqrt(n)) + 1, 2) :
+        if (n % i) == 0 :
+            d[n] = False
+            return False
+    d[n] = True
+    return True
+
+
+if __name__ == "__main__" :
+    firstLine = True;
+    lineIter = iter(stdin)
+
+    numLines = int(next(lineIter))
+
+    for v in lineIter :
+        s = v.split(" ")
+        i = int (s[0])
+        j = int (s[1])
+
+        if j < i :
+          for r in range(j,i+1) :
+            if(is_prime(r)) :
+               print(r);
+          print("")
+        else :
+          for r in range(i,j+1) :
+            if(is_prime(r)) :
+               print(r);
+          print("")
+
+        if(numLines == 1) :
+           break
+        else:
+           numLines -= 1
+
+'''
+    for v in stdin :
+        if(firstLine) :
+           numLines = int(v)
+           firstLine = False
+        else:
+           s = v.split(" ")
+           i = int (s[0])
+           j = int (s[1])
+
+           if j < i :
+             for r in range(j,i+1) :
+               if(is_prime(r)) :
+                  print(r);
+             print("")
+           else :
+             for r in range(i,j+1) :
+               if(is_prime(r)) :
+                  print(r);
+             print("")
+
+           if(numLines == 1) :
+              break
+           else:
+              numLines -= 1
+'''
+
+
+
+
